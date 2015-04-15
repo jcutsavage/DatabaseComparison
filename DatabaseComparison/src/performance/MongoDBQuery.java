e92f85b4bbcf71f57bd5cbc4413e8e1145777913
John Cutsavage <anikansky3@gmail.com>
2015-04-15 17:35:01 -0400

    Began work on Query classes.

--- DatabaseComparison/src/performance/MongoDBQuery.java
+++ DatabaseComparison/src/performance/MongoDBQuery.java
@@ -1,5 +1,16 @@
 package performance;
 
+/**
+ * 
+ * @author John Cutsavage
+ * @author Sharmeen Jahan
+ * @author Drew Whittaker
+ *
+ * MongoDB implementation of Query class.
+ */
 public class MongoDBQuery extends Query {
 	
+	public void run(){
+		
+	}
 }
--- DatabaseComparison/src/performance/Query.java
+++ DatabaseComparison/src/performance/Query.java
@@ -1,5 +1,19 @@
 package performance;
 
+/**
+ * 
+ * @author John Cutsavage
+ * @author Sharmeen Jahan
+ * @author Drew Whittaker
+ * 
+ * Abstract class for handling database queries
+ *
+ */
 public abstract class Query {
 	
+	/**
+	 * Execute query and print out the resulting
+	 * table
+	 */
+	public void run(){}
 }
