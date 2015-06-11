2e133d257df740d8454ed741fbae3670e5073ec4
Sharmeenjahan <jahans20@students.rowan.edu>
2015-06-11 17:07:03 -0400

    Update Driver.java

--- DatabaseComparison/src/performance/NewQueries/Driver.java
+++ DatabaseComparison/src/performance/NewQueries/Driver.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public class Driver{
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, ParseException {
 		
 		Query sqlQuery= new MysqlQueries();
 		sqlQuery.initConnection();
