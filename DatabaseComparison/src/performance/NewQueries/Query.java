9c2b6137e185af65e69f8a249eaa153866c56b06
Sharmeenjahan <jahans20@students.rowan.edu>
2015-06-15 13:19:59 -0400

    Update Query.java

--- DatabaseComparison/src/performance/NewQueries/Query.java
+++ DatabaseComparison/src/performance/NewQueries/Query.java
@@ -6,7 +6,9 @@ public abstract class Query {
 	public void executeQuery(){}
 	public abstract void initConnection();
 	public abstract void empNoOfCertainSalary(int salary);
-	public abstract void avgSalaryAfterDate(String date);
+	public abstract void avgSalary(String date);
 	public abstract void avgSalaryAfterDate(Date date);
 	public abstract void avgSalary();
+	
+	
 }
