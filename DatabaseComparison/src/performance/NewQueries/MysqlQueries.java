6e513c58bc1d751fba13c9b7362b82d57bcf0a44
Sharmeenjahan <jahans20@students.rowan.edu>
2015-06-22 12:48:53 -0400

    Update MysqlQueries.java

--- DatabaseComparison/src/performance/NewQueries/MysqlQueries.java
+++ DatabaseComparison/src/performance/NewQueries/MysqlQueries.java
@@ -190,6 +190,91 @@ public class MysqlQueries extends Query {
 	}
 	
 	/**
+	 * Get the execution time of finding first name, last name and salary of all employees joining two tables 
+	 * employees and salaries. With and without indexes.
+	 */
+	public void empNameAndSalary(){
+		try{
+
+			Statement statement = con.createStatement();
+			Statement chkSalStatement= con.createStatement();
+			Statement salIndexStatement = con.createStatement();
+
+			//check if there is any existing index on salary table.
+			String chksalSql= "select count(1) IndexIsThere from information_schema.statistics "
+					+ "where table_schema= 'employees' and table_name= 'salaries' and index_name= 'sal_emp_no'";
+			ResultSet resIndex = chkSalStatement.executeQuery(chksalSql); 
+			resIndex.next();
+			//if there is no existing index, create one on salaries emp_no.
+			if(resIndex.getInt(1) == 0){
+				String indexSql= "create index sal_emp_no on salaries(emp_no)";
+				salIndexStatement.executeUpdate(indexSql);
+			}
+
+			Statement chkEmpStatement= con.createStatement();
+			Statement empIndexStatement = con.createStatement();
+			//check if there is any index on employees table.
+			String chkEmpSql= "select count(1) IndexIsThere from information_schema.statistics where"
+					+ " table_schema= 'employees' and table_name= 'employees' and index_name= 'emp_emp_no'";
+			ResultSet resEmpIndex=chkEmpStatement.executeQuery(chkEmpSql);
+			resEmpIndex.next(); 
+			//if not, create one on employees emp_no.
+			if(resEmpIndex.getInt(1) == 0){
+				String empIndexSql= "create index emp_emp_no on employees(emp_no)";
+				empIndexStatement.executeUpdate(empIndexSql);
+			}
+
+			//Evaluating sql execution time with two indexes...
+			//taking the time before executing query.
+			long startTime = System.currentTimeMillis();
+			//the query to find the first name, last name and salary of each employee with both indexes
+			String sql="select e.first_name, e.last_name, s.salary from employees as e "
+					+ "use index(emp_emp_no) inner join salaries as s use index(sal_emp_no) "
+					+ "on e.emp_no= s.emp_no";
+			res = statement.executeQuery(sql);
+			//taking the time after executing query.
+			long endTime = System.currentTimeMillis();
+			//time takes to execute query.
+			long runTime = endTime - startTime;
+			System.out.println("empNameAndSalary with Index on both employees and salaries tables");
+			System.out.println("Time to execute = " + runTime + " milliseconds\n");
+
+			//Evaluating execution time with one Index...
+			//taking the time before executing query.
+			long startTime1 = System.currentTimeMillis();
+			//the query to find the first name, last name and salary of each employee with index on employees.
+			String sql1="select e.first_name, e.last_name, s.salary from employees as e use index(emp_emp_no)"
+					+ " inner join salaries as s on e.emp_no= s.emp_no";
+			ResultSet res1= statement.executeQuery(sql1);
+			//taking the time after executing query.
+			long endTime1 = System.currentTimeMillis();
+			//time takes to execute query.
+			long runTime1 = endTime1 - startTime1;
+			System.out.println("empNameAndSalary with Index on only employees table");
+			System.out.println("Time to execute = " + runTime1 + " milliseconds\n");
+
+			//Evaluating execution time without any indexes...
+			//taking the time before executing query.
+			long startTime2 = System.currentTimeMillis();
+			//the query to find the first name, last name and salary of each employee without using any index.
+			String sql2="select e.first_name, e.last_name, s.salary from employees as e "
+					+ " inner join salaries as s on e.emp_no= s.emp_no";
+			ResultSet res2= statement.executeQuery(sql2);
+			//taking the time after executing query.
+			long endTime2 = System.currentTimeMillis();
+			//time takes to execute query.
+			long runTime2 = endTime2 - startTime2;
+			System.out.println("empNameAndSalary without any Indexes");
+			System.out.println("Time to execute = " + runTime2 + " milliseconds\n");
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	/**
 	 * Reset the query cache for MySQL so queries can
 	 * be timed as if they were being run for the first time.
 	 */
