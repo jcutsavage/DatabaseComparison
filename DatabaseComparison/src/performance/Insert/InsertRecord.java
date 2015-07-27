13a6ad3375c6ff91d8a4d85b0d1165e3b2565fc6
Sharmeenjahan <jahans20@students.rowan.edu>
2015-07-27 16:16:45 -0400

    Update InsertRecord.java

--- DatabaseComparison/src/performance/Insert/InsertRecord.java
+++ DatabaseComparison/src/performance/Insert/InsertRecord.java
@@ -2,12 +2,11 @@ package performance.Insert;
 
 import java.util.Date;
 
-public abstract class Abs_insert
+public abstract class InsertRecord
 
 {
 	public void executeQuery(){}
 	public abstract void initConnection();
   	public abstract void insertEmployee(int emp_no, int salary, String fromDate, String toDate);
-	public abstract void resetCache();
 	
 }
