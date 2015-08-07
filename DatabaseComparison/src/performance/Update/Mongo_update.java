0d484e86f046a2c35b061b6dd5f27cf728f1ad86
Sharmeenjahan <jahans20@students.rowan.edu>
2015-08-07 15:53:58 -0400

    Update Mongo_update.java

--- DatabaseComparison/src/performance/Update/Mongo_update.java
+++ DatabaseComparison/src/performance/Update/Mongo_update.java
@@ -34,19 +34,16 @@ public class Mongo_update extends Abs_update {
 		}
 	}
 	
-  	@Override
-	public void updateEmployee(int emp_no, int salary, int newSalary,
-			String fromDate, String toDate) {
-		// TODO Auto-generated method stub
-
-	}
+  	
 
 	//update a document in the salaries collection using keys emp_no and from_date.
 	//@param employee number, salary, from date and to date of the existing document and 
 	//the new salary that we want to update.
-	public void updateEmployee(int emp_no, int salary, int newSalary,
-			Date fromDate, Date toDate) {
-
+	//update a document in the salaries collection using keys emp_no and from_date.
+	//@param employee number, salary, from date and to date of the existing document and 
+	//the new salary that we want to update.
+	public void updateEmployee(int emp_no, int salary, int newSalary,Date fromDate, Date toDate) {
+		
 		//taking the time before executing query.
 		long startTime = System.currentTimeMillis();
 
@@ -64,15 +61,16 @@ public class Mongo_update extends Abs_update {
 			DBCursor cursor1 = sColl.find(checkQuery);
 
 			if (!cursor1.hasNext()){
-				System.err.println("Error: collection does not have this record to update.");
+				System.err.println("Error while update: collection does not have this record to update.");
+
 			}
 			else {
 				BasicDBObject oldRow = new BasicDBObject("emp_no",id).append("salary", salary).append("from_date", fromDate).append("to_date", toDate);
+
 				BasicDBObject newRow = new BasicDBObject("emp_no",id).append("salary", newSalary).append("from_date", fromDate).append("to_date", toDate);
 
 				sColl.update(oldRow, newRow);
 				System.out.println(newRow);
-				System.out.println("successfull");
 			}
 		}
 		//taking the time after executing query.
@@ -81,8 +79,19 @@ public class Mongo_update extends Abs_update {
 		//time takes to execute query.
 		long runTime = endTime - startTime;
 		System.out.println();
-		System.out.println("Time to execute = " + runTime + " milliseconds");        
+		System.out.println("(Update) Time to execute = " + runTime + " milliseconds");        
 
+	}
+	
+	@Override
+	public void updateEmployee(int emp_no, int salary, int newSalary, String fromDate, String toDate) {
+		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
 	}
 }
