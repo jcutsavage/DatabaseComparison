19ab0a8d288dccaab44b43de59f89fa55e0550af
Sharmeenjahan <jahans20@students.rowan.edu>
2015-08-07 15:56:31 -0400

    Update and rename Abs_update to Abs_update.java

--- DatabaseComparison/src/performance/Update/Abs_update.java
+++ DatabaseComparison/src/performance/Update/Abs_update.java
@@ -7,5 +7,5 @@ public abstract class Abs_update {
 	public abstract void initConnection();
 	public abstract void updateEmployee(int emp_no, int salary, int newSalary, String fromDate, String toDate);
 	public abstract void updateEmployee(int emp_no, int salary, int newSalary, Date fromDate, Date toDate);
-	
+	public abstract void flush();
 }
