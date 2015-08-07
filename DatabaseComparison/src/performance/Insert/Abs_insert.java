95478c2743dbdfd76b5284992659ed7ec83ebcef
Sharmeenjahan <jahans20@students.rowan.edu>
2015-08-07 15:34:19 -0400

    Update Abs_insert.java

--- DatabaseComparison/src/performance/Insert/Abs_insert.java
+++ DatabaseComparison/src/performance/Insert/Abs_insert.java
@@ -5,8 +5,8 @@ import java.util.Date;
 public abstract class Abs_insert
 
 {
-	public void executeQuery(){}
 	public abstract void initConnection();
   	public abstract void insertEmployee(int emp_no, int salary, String fromDate, String toDate);
-	
+	public abstract void insertEmployee(int emp_no, int salary, Date fromDate, Date toDate);
+	public abstract void flush();
 }
